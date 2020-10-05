dfbf1afb1d26ca247c3b0a66182e7c06eb76d7ee
harsha-p <harsha.pathuri@students.iiit.ac.in>
2020-10-05 16:26:02 +0530

    need to do constraints , search

--- project.py
+++ project.py
@@ -1,4 +1,3 @@
-
 import subprocess as sp
 import pymysql
 import pymysql.cursors
@@ -60,11 +59,11 @@ def view():
     elif ch == '6':
         query = "SELECT * FROM TICKET_COST"
     elif ch == '7':
-        query = "SELECT fname,lname,sex, from STAFF where position=\"Manager\""
+        query = """SELECT fname,lname,sex from STAFF where position="Manager";"""
     elif ch == '8':
-        query = "SELECT fname,lname,sex, from STAFF where position=\"Maintainer\""
+        query = """SELECT fname,lname,sex from STAFF where position="Maintainer";"""
     elif ch == '9':
-        query = "SELECT fname,lname,sex, from STAFF where position=\"Operator\""
+        query = """SELECT fname,lname,sex from STAFF where position="Operator";"""
     elif ch == '10':
         ticketid = int(input("TICKET ID: "))
         query = "SELECT SUM(photo_cost) as TOTAL_COST FROM ((SELECT photo_size FROM PHOTO WHERE ticket_id=%d) as P INNER JOIN PHOTO_COST as C ON P.photo_size=C.photo_size)" % (ticketid)
@@ -80,7 +79,8 @@ def view():
         query = "select D.attraction_id as ATTRACTION from (select COUNT(*) as count,attraction_id from VISITED_ATTRACTIONS group by attraction_id) as D inner join (select AVG(count) as avg from (select COUNT(*) as count from VISITED_ATTRACTIONS group by attraction_id) as A ) as B ON D.count > B.avg"
     else:
         print("You have entered an invalid option.")
-
+        return
+    print(query)
     try:
         no_of_rows = cur.execute(query)
     except Exception as e:
@@ -99,15 +99,15 @@ def update():
     print("2.   Visitor")
     print("3.   Photo")
     print("4.   Attraction")
-    print("5.   Ticket")
+    print("5.   Maintainance Schedule")
     print("\n")
     ch = int(input("Enter: "))
     if ch == 1:
         l = 0
         query = "UPDATE STAFF "
         query2 = "WHERE "
-        row = ["id", "date_of_birth", "join_date", "fname", "lname",
-               "sex", "attraction", "salary", "door_number", "street", "pincode"]
+        row = ["id", "fname", "lname", "sex", "date_of_birth", "join_date", "working_hours",
+               "position", "salary", "door_no", "street", "pincode", "attraction_id"]
         for i in row:
             if input('update %s (y/n): ' % (i)) == 'y':
                 inp = input("Enter %s: " % (i))
@@ -116,13 +116,10 @@ def update():
                     l = 1
                 else:
                     query += "%s=\"%s\"," % (i, inp)
-            elif input('use as WHERE clause %s (y/n): ' % (i)) == 'y':
+        for i in row:
+            if input('use as condition %s (y/n): ' % (i)) == 'y':
                 inp = input("Enter %s: " % (i))
-                query2 += "%s=\"%s\" " % (i, inp)
-        query = query[:-1]
-        query += " "
-        query += query2
-        query += ";"
+                query2 += "%s=\"%s\" AND " % (i, inp)
     elif ch == 2:
         l = 0
         query = "UPDATE VISITOR "
@@ -136,14 +133,10 @@ def update():
                     l = 1
                 else:
                     query += "%s=\"%s\"," % (i, inp)
-            elif input('use as WHERE clause %s (y/n): ' % (i)) == 'y':
+        for i in row:
+            if input('use as condition %s (y/n): ' % (i)) == 'y':
                 inp = input("Enter %s: " % (i))
-                query2 += "%s=\"%s\" " % (i, inp)
-        query = query[:-1]
-        query += " "
-        query += query2
-        query += ";"
-        print(query)
+                query2 += "%s=\"%s\" AND " % (i, inp)
     elif ch == 3:
         l = 0
         query = "UPDATE PHOTO "
@@ -157,35 +150,72 @@ def update():
                     l = 1
                 else:
                     query += "%s=\"%s\"," % (i, inp)
-            elif input('use as WHERE clause %s (y/n): ' % (i)) == 'y':
+        for i in row:
+            if input('use as condition %s (y/n): ' % (i)) == 'y':
                 inp = input("Enter %s: " % (i))
-                query2 += "%s=\"%s\" " % (i, inp)
-        query = query[:-1]
-        query += " "
-        query += query2
-        query += ";"
+                query2 += "%s=\"%s\" AND " % (i, inp)
+    elif ch == 4:
+        l = 0
+        query = "UPDATE ATTRACTION "
+        query2 = "WHERE "
+        row = ["attraction_id", "name", "for_adult", "for_child"]
+        for i in row:
+            if input('update %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                if l == 0:
+                    query += "SET %s=\"%s\"," % (i, inp)
+                    l = 1
+                else:
+                    query += "%s=\"%s\"," % (i, inp)
+        for i in row:
+            if input('use as condition %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                query2 += "%s=\"%s\" AND " % (i, inp)
+    elif ch == 5:
+        l = 0
+        query = "UPDATE MAINTAINANCE_SCHEDULE "
+        query2 = "WHERE "
+        row = ["id", "attraction_id", "start_time", "end_time"]
+        for i in row:
+            if input('update %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                if l == 0:
+                    query += "SET %s=\"%s\"," % (i, inp)
+                    l = 1
+                else:
+                    query += "%s=\"%s\"," % (i, inp)
+        for i in row:
+            if input('use as condition %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                query2 += "%s=\"%s\" AND " % (i, inp)
     else:
         print("You have entered an invalid option.")
-
+        return
+    query = query[:-1]
+    query += " "
+    query2 = query2[0:-4]
+    query += query2
+    print(query)
     try:
         cur.execute(query)
         con.commit()
         print("Updated in Database")
     except Exception as e:
+        print(e)
         con.rollback()
-        print("Failed to insert into database")
+        print("Failed to update database")
         print(">>>>>>>>>>>>>", e)
 
     return
 
 
 def insert():
-    # print("\nChoose the data that you want to insert.\n\n")
     print("1.   Visitor")
     print("2.   Staff")
     print("3.   Attraction")
     print("4.   Ticket")
     print("5.   Photo")
+    print("6.   Maintainance Schedule")
     print("\n")
     ch = input("Enter: ")
     if ch == '1':
@@ -220,7 +250,6 @@ def insert():
             row["id"], row["fname"], row["lname"], row["sex"], row["dob"], row["jd"], row[
                 "wh"], row["position"], row["salary"], row["do"], row["street"], row["pin"], row["wa"]
         )
-        print(query)
     elif ch == '3':
         row = {}
         print("Enter attraction details: ")
@@ -243,7 +272,6 @@ def insert():
             row["id"], row["ssn"], row["ticket_type"], row["sid"]
         )
     elif ch == '5':
-        print(":omvfm")
         row = {}
         print("Enter photo details: ")
         row["ticket_id"] = input("Ticket ID: ")
@@ -251,9 +279,20 @@ def insert():
         row["photo_size"] = input("Photo size: ")
         query = "INSERT INTO PHOTO(ticket_id,attraction_id,photo_size) VALUES ('%s','%s','%s')" % (
                 row["ticket_id"], row["attraction_id"], row["photo_size"])
+    elif ch == '6':
+        row = {}
+        print("Enter Maintainance Schedule Details: ")
+        row["id"] = input("Enter Staff ID: ")
+        row["attraction_id"] = input("Enter Attraction ID: ")
+        row["starttime"] = input("Enter Start time (YYYY-MM-DD HH:MM:SS): ")
+        row["endtime"] = input("Enter End time (YYYY-MM-DD HH:MM:SS): ")
+        query = "INSERT INTO MAINTAINANCE_SCHEDULE(maintainer,attraction_id,start_time,end_time) VALUES ('%s','%s','%s','%s') " % (
+            row["id"], row["attraction_id"], row["starttime"], row["endtime"]
+        )
     else:
         print("You have entered an invalid option.")
-
+        return
+    print(query)
     try:
         cur.execute(query)
         con.commit()
@@ -266,15 +305,124 @@ def insert():
     return
 
 
-def dispatch(ch):
+def owncommand():
+
+    query = input("Enter Command: \n")
+    try:
+        no_of_rows = cur.execute(query)
+    except Exception as e:
+        print(e)
+        con.rollback()
+        print("Failed to execute")
+        print(">>>>>>>>>>>>>", e)
+        return
+
+    rows = cur.fetchall()
+    viewTable(rows)
+    con.commit()
+
+    return
+
+
+def delete():
+
+    print("\nChoose the data that you want to delete.\n\n")
+    print("1.   Staff")
+    print("2.   Visitor")
+    print("3.   Photo")
+    print("4.   Maintainance Schedule")
+    print("\n")
+    ch = int(input("Enter: "))
+    if ch == 1:
+        l = 0
+        query = "DELETE FROM STAFF WHERE "
+        row = ["id", "date_of_birth", "join_date", "fname", "lname",
+               "sex", "attraction_id", "salary", "door_number", "street", "pincode"]
+        for i in row:
+            if input('Use %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                if l == 0:
+                    query += " %s=\"%s\" " % (i, inp)
+                    l = 1
+                else:
+                    query += " AND %s=\"%s\" " % (i, inp)
+    elif ch == 2:
+        l = 0
+        query = "DELETE FROM VISITOR WHERE "
+        row = ["ssn", "date_of_birth", "fname", "lname", "sex"]
+        for i in row:
+            if input('Use %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                if l == 0:
+                    query += " %s=\"%s\" " % (i, inp)
+                    l = 1
+                else:
+                    query += " AND %s=\"%s\" " % (i, inp)
+    elif ch == 3:
+        l = 0
+        query = "DELETE FROM PHOTO WHERE "
+        row = ["ticket_id", "attraction_id", "photo_time", "photo_size"]
+        for i in row:
+            if input('Use %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                if l == 0:
+                    query += " %s=\"%s\" " % (i, inp)
+                    l = 1
+                else:
+                    query += " AND %s=\"%s\" " % (i, inp)
+    elif ch == 4:
+        l = 0
+        query = "DELETE FROM MAINTAINANCE_SCHEDULE WHERE "
+        row = ["maintainer", "attraction_id", "start_time", "end_time"]
+        for i in row:
+            if input('Use %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                if l == 0:
+                    query += " %s=\"%s\" " % (i, inp)
+                    l = 1
+                else:
+                    query += " AND %s=\"%s\" " % (i, inp)
+    elif ch == 6:
+        l = 0
+        query = "DELETE FROM MAINTAINANCE_SCHEDULE WHERE "
+        row = ["maintainer", "attraction_id", "start_time", "end_time"]
+        for i in row:
+            if input('Use %s (y/n): ' % (i)) == 'y':
+                inp = input("Enter %s: " % (i))
+                if l == 0:
+                    query += " %s=\"%s\" " % (i, inp)
+                    l = 1
+                else:
+                    query += " AND %s=\"%s\" " % (i, inp)
+    else:
+        print("You have entered an invalid option.")
+        return
+
+    print(query)
+    try:
+        cur.execute(query)
+        con.commit()
+        print("Updated in Database")
+    except Exception as e:
+        print(e)
+        con.rollback()
+        print("Failed to update database")
+        print(">>>>>>>>>>>>>", e)
+
+    return
+
+
+def check(ch):
     if(ch == 1):
         view()
     elif(ch == 2):
         insert()
     elif(ch == 3):
         update()
-    # elif(ch == 4):
-    #     delete()
+    elif(ch == 4):
+        delete()
+    elif(ch == 5):
+        owncommand()
     else:
         print("Error: Invalid Option")
 
@@ -321,14 +469,15 @@ while(1):
                 print("2.   Addition Options")
                 print("3.   Update Options")
                 print("4.   Delete Options")
-                print("5.   Logout")
-                ch = int(input("\nEnter ch: "))
-                if(ch == 5):
+                print("5.   Enter Own Commad")
+                print("6.   Logout")
+                ch = int(input("\nEnter: "))
+                if(ch == 6):
                     print("Exiting.")
                     exit = 1
                     break
                 else:
-                    dispatch(ch)
+                    check(ch)
                     tmp = input("Enter any key to CONTINUE>")
         if exit == 1:
             break
